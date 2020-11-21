d7e6980708df0b097d7166a90e7e3fe7d2606c6c
jinwoo <jintakk6531@gmail.com>
2020-11-21 17:53:22 +0900

    11/21

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
-import Head from 'next/head';
+import Layout from '../components/Layout/Layout';
 import styles from '../styles/Home.module.css';
+import SearchInput from '../components/SearchInput/SearchInput';
 
-export default function Home() {
+export default function Home({ countries }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next Appdd</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <main className={styles.main}>main</main>
-
-      <footer className={styles.footer}>footer</footer>
-    </div>
+      <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+    </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
