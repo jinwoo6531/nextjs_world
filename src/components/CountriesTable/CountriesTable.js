acdac9fdd7b8b5ca50e9e0f6768c62fe132b264f
jinwoo <jintakk6531@gmail.com>
2020-12-07 16:45:29 +0900

    12/07

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import Link from 'next/link';
 import {
   KeyboardArrowDownRounded,
   KeyboardArrowUpRounded,
 } from '@material-ui/icons';
+import { useState } from 'react';
 import styles from './CountriesTable.module.css';
 
 const orderBy = (countries, value, direction) => {
@@ -13,6 +14,8 @@ const orderBy = (countries, value, direction) => {
   if (direction === 'desc') {
     return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
   }
+
+  return countries;
 };
 
 const SortArrow = ({ direction }) => {
@@ -59,12 +62,15 @@ const CountriesTable = ({ countries }) => {
   return (
     <div>
       <div className={styles.heading}>
+        <div className={styles.heading_flag}></div>
+
         <button
           className={styles.heading_name}
           onClick={() => setValueAndDirection('name')}
         >
           <div>Name</div>
-          <SortArrow />
+
+          {value === 'name' && <SortArrow direction={direction} />}
         </button>
 
         <button
@@ -72,15 +78,46 @@ const CountriesTable = ({ countries }) => {
           onClick={() => setValueAndDirection('population')}
         >
           <div>Population</div>
-          <SortArrow direction={direction} />
+
+          {value === 'population' && <SortArrow direction={direction} />}
+        </button>
+
+        <button
+          className={styles.heading_area}
+          onClick={() => setValueAndDirection('area')}
+        >
+          <div>
+            Area (km<sup style={{ fontSize: '0.5rem' }}>2</sup>)
+          </div>
+
+          {value === 'area' && <SortArrow direction={direction} />}
+        </button>
+
+        <button
+          className={styles.heading_gini}
+          onClick={() => setValueAndDirection('gini')}
+        >
+          <div>Gini</div>
+
+          {value === 'gini' && <SortArrow direction={direction} />}
         </button>
       </div>
 
       {orderedCountries.map((country) => (
-        <div className={styles.row}>
-          <div className={styles.name}>{country.name}</div>
-          <div className={styles.population}>{country.population}</div>
-        </div>
+        <Link href={`/country/${country.alpha3Code}`} key={country.name}>
+          <div className={styles.row}>
+            <div className={styles.flag}>
+              <img src={country.flag} alt={country.name} />
+            </div>
+            <div className={styles.name}>{country.name}</div>
+
+            <div className={styles.population}>{country.population}</div>
+
+            <div className={styles.area}>{country.area || 0}</div>
+
+            <div className={styles.gini}>{country.gini || 0} %</div>
+          </div>
+        </Link>
       ))}
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import Layout from '../components/Layout/Layout';
-import styles from '../styles/Home.module.css';
-import SearchInput from '../components/SearchInput/SearchInput';
+import Head from 'next/head';
+import { useState } from 'react';
 import CountriesTable from '../components/CountriesTable/CountriesTable';
+import Layout from '../components/Layout/Layout';
+import SearchInput from '../components/SearchInput/SearchInput';
+import styles from '../styles/Home.module.css';
 
 export default function Home({ countries }) {
   const [keyword, setKeyword] = useState('');
 
-  const filterCountries = countries.filter(
+  const filteredCountries = countries.filter(
     (country) =>
       country.name.toLowerCase().includes(keyword) ||
       country.region.toLowerCase().includes(keyword) ||
@@ -15,17 +17,24 @@ export default function Home({ countries }) {
 
   const onInputChange = (e) => {
     e.preventDefault();
+
     setKeyword(e.target.value.toLowerCase());
   };
+
   return (
     <Layout>
-      <div className={styles.counts}>Found {countries.length} countriess</div>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput
-        placeholder="Filter by Name, Region or SubRegion"
-        onChnage={onInputChange}
-      />
-      <CountriesTable countries={filterCountries} />
+        <div className={styles.input}>
+          <SearchInput
+            placeholder="Filter by Name, Region or SubRegion"
+            onChange={onInputChange}
+          />
+        </div>
+      </div>
+
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
